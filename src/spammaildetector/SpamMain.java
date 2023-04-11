ad4725543209c0923a0a332170f356ebba2e20b6
Jibin Mathew Peechatt <jibin@email.com>
2023-04-11 18:42:55 +0200

    Final Setup of main class

--- src/spammaildetector/SpamMain.java
+++ src/spammaildetector/SpamMain.java
@@ -1,5 +1,26 @@
 package spammaildetector;
 
-public class SpamMain {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SpamMain extends Application {
+	private SpamModel model;
+	private SpamView view;
+	private SpamController controller;
+	
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		model = new SpamModel();
+		view = new SpamView(primaryStage, model);
+		controller = new SpamController(model, view);
+		view.start();
+
+	}
+
+	public static void main(String[] args) {
+		launch();
+
+	}
 
 }
