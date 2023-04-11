e74ecce4c7cb575f1a4087571c9e0ab0edddc55d
Jibin Mathew Peechatt <jibin@email.com>
2023-04-11 18:44:23 +0200

    Basic setup of Controller

---
+++ src/spammaildetector/SpamController.java
@@ -0,0 +1,12 @@
+package spammaildetector;
+
+public class SpamController {
+	private SpamModel model;
+	private SpamView view;
+
+	public SpamController(SpamModel model, SpamView view) {
+		this.model = model;
+		this.view = view;
+	}
+
+}
